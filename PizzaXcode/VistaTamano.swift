394ab87c63494d3db40fd38fd19674db05e36a2a
Wyz3r <leonelislasglez@gmail.com>
2016-02-03 02:10:45 -0600

    primer commit junto con 2 vistas

--- PizzaXcode/VistaTamano.swift
+++ PizzaXcode/VistaTamano.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VistaTamano.swift
 //  PizzaXcode
 //
 //  Created by Leo on 03/02/16.
@@ -8,21 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class VistaTamano: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let backgroundImage = UIImageView(frame: UIScreen.mainScreen().bounds)
         backgroundImage.image = UIImage(named: "rustica_background")
         self.view.insertSubview(backgroundImage, atIndex: 0)
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
