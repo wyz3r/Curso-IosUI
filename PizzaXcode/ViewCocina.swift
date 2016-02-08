2eea88463d9f50ab506ae495e1e47a8a217ad8e6
Wyz3r <leonelislasglez@gmail.com>
2016-02-07 23:21:36 -0600

    fase gráfica casi terminada funcionalidad 90%

---
+++ PizzaXcode/ViewCocina.swift
@@ -0,0 +1,37 @@
+//
+//  ViewCocina.swift
+//  PizzaXcode
+//
+//  Created by Leo on 07/02/16.
+//  Copyright © 2016 leonel islas gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewCocina: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let backgroundImage = UIImageView(frame: UIScreen.mainScreen().bounds)
+        backgroundImage.image = UIImage(named: "rustica_background")
+        self.view.insertSubview(backgroundImage, atIndex: 0)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
